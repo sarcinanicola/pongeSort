d4876221536f180f743415c08d27fa379d477476
sarcina.nicola <sarcina.nicola@TAVE-PC31.OSDBSESTO.local>
2021-04-29 12:15:22 +0200

    add ramndomly filled char array

--- src/pongesort/PongeSort.java
+++ src/pongesort/PongeSort.java
@@ -5,17 +5,19 @@
  */
 package pongesort;
 
+import java.util.Scanner;
+
 /**
  *
  * @author pogliani.mattia
  */
 public class PongeSort {
+ public static char[] lista;
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
+       Scanner sc = new Scanner(System.in);
+       char [] arr = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'};
     }
     
 }
